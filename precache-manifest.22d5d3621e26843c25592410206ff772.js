12f2a954f23c35ea3abfa026e8b6cc77418d60e1
Rommel <rommel@myretailplus.com>
2020-03-31 06:51:33 +0800

    Updates

---
+++ precache-manifest.22d5d3621e26843c25592410206ff772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af1577c7499c4dc7c2194ecfefa3b38",
+    "url": "/covid19-tracker.github.io/index.html"
+  },
+  {
+    "revision": "62a6eb82639cb7922bef",
+    "url": "/covid19-tracker.github.io/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "500c42ae33d81fe91114",
+    "url": "/covid19-tracker.github.io/static/css/main.f8eaac66.chunk.css"
+  },
+  {
+    "revision": "62a6eb82639cb7922bef",
+    "url": "/covid19-tracker.github.io/static/js/2.c812fb71.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19-tracker.github.io/static/js/2.c812fb71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500c42ae33d81fe91114",
+    "url": "/covid19-tracker.github.io/static/js/main.472b5c95.chunk.js"
+  },
+  {
+    "revision": "75d4be11e4f6618fa1c2",
+    "url": "/covid19-tracker.github.io/static/js/runtime-main.c065b602.js"
+  }
+]);
