6f730c77a483d0f6f8d9a1a52137fc75db947d39
Rommel <rommel@myretailplus.com>
2020-04-13 00:17:44 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-tracker/precache-manifest.ddcedf1a1111321765d13a80d600ac53.js"
+  "/covid19-tracker/precache-manifest.49decb298d17014be6b3ab024501533a.js"
 );
 
 self.addEventListener('message', (event) => {
